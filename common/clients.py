453de060f9587c695540f7e9a98a038b85821776
Josue <ochoajbusiness@gmail.com>
2025-02-23 11:37:32 -0800

    Updated get_anthropic_client as singleton instance (prevent reinstantiations)

--- common/clients.py
+++ common/clients.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+_anthropic_client = None
+
 def get_anthropic_client():
-    """Creates and returns a global Anthropic client instance."""
-    return AsyncAnthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+    """Returns a single global instance of the Anthropic client."""
+    global _anthropic_client
+    if _anthropic_client is None:
+        _anthropic_client = AsyncAnthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+    return _anthropic_client
