290851988368ac04f6c78169369709294fe7e512
Josue <ochoajbusiness@gmail.com>
2025-03-09 14:35:24 -0700

    Reformat with black

--- scripts/common/clients.py
+++ scripts/common/clients.py
@@ -5,19 +5,25 @@ from common.config import ANTHROPIC_API_KEY
 
 load_dotenv()
 
+
 class Singleton(type):
     _instances = {}
+
     def __call__(class_, *args, **kwargs):
         if class_ not in class_._instances:
-            class_._instances[class_] = super(Singleton, class_).__call__(*args, **kwargs)
+            class_._instances[class_] = super(Singleton, class_).__call__(
+                *args, **kwargs
+            )
         return class_._instances[class_]
-    
+
+
 class AnthropicClient(metaclass=Singleton):
     def __init__(self):
         self.client = AsyncAnthropic(api_key=ANTHROPIC_API_KEY)
 
     def get_client(self):
         return self.client
+
 
 def get_anthropic_client():
     return AnthropicClient().get_client()
