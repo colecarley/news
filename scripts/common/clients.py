da1d8cce4c929e2d5b4519e01a252be1dcfbf637
Josue <ochoajbusiness@gmail.com>
2025-03-09 14:34:49 -0700

    renamed cls -> class_

--- scripts/common/clients.py
+++ scripts/common/clients.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 from common.config import ANTHROPIC_API_KEY
 
 load_dotenv()
+
 class Singleton(type):
     _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+    def __call__(class_, *args, **kwargs):
+        if class_ not in class_._instances:
+            class_._instances[class_] = super(Singleton, class_).__call__(*args, **kwargs)
+        return class_._instances[class_]
     
 class AnthropicClient(metaclass=Singleton):
     def __init__(self):
